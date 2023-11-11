ea0345f6ae76d06dc0f2a37bd82b3fe30610ef17
precieu26 <precieudieudonne@gmail.com>
2023-11-10 19:46:27 -0500

    creation of the first validation but is  for numeric i want just integrer

--- pages/result.php
+++ pages/result.php
@@ -1,1 +1,21 @@
-<h1>Bonjour</h1>
+
+<head>
+    <link rel="stylesheet" href="../style/monstyle.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body class="affichage2">
+    <?php
+    //j'effectue ma premiere  validation qui verras si l'element envoyer et un entier
+    if(is_numeric($_POST['Nb_adresse'])){
+        echo"bonjour";
+    }else{
+        echo"echouer";
+    }
+    echo"<br><br>";
+    //var_dump($_POST['Nb_adresse']);
+    
+    
+    ?>
+</body>
